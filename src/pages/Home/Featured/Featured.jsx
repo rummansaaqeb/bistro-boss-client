57cdb8fecf1632c955c8fd80ba3b7e315e3fb62d
rummansaaqeb <rsaaqeb@gmail.com>
2025-01-11 14:40:37 +0600

    created parallax and added testimonials section with react rating

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -5,9 +5,9 @@ import './Featured.css';
 
 const Featured = () => {
     return (
-        <div className='featured-item text-white pt-8 my-20'>
+        <div className='featured-item bg-fixed text-white pt-8 my-20'>
             <SectionTitle heading="Featured Item" subHeading="Check It Out"></SectionTitle>
-            <div className='md:flex justify-center items-center px-36 pb-20 pt-12'>
+            <div className='md:flex justify-center items-center bg-slate-500 bg-opacity-40 px-36 pb-20 pt-12'>
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -15,7 +15,7 @@ const Featured = () => {
                     <p>Aug 20, 2029</p>
                     <p className='uppercase'>Where can i get some?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti delectus, voluptas et ipsum vel, magnam maxime explicabo nisi, natus autem repellendus dignissimos laboriosam officia impedit repellat tempore corrupti eveniet laborum est veritatis quisquam beatae optio a esse! Error, fugiat veniam praesentium quasi libero fugit eveniet quis et, cum ex consectetur.</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
                 </div>
             </div>
         </div>
---
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,49 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
+import { FaQuoteLeft } from "react-icons/fa";
+
+
+const Testimonials = () => {
+    const [reviews, setReviews] = useState([]);
+    useEffect(() => {
+        fetch('./reviews.json')
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data);
+            })
+    }, [])
+
+    const ratingStyles = {
+    }
+
+    return (
+        <section className="my-20">
+            <SectionTitle subHeading="What Our Client Says" heading="Testimonials"></SectionTitle>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                {
+                    reviews.map(review => <SwiperSlide key={review._id}>
+                        <div className="flex flex-col items-center my-16 mx-24">
+                            <Rating
+                                style={{ maxWidth: 180 }}
+                                className="rating"
+                                value={review.rating}
+                                readOnly
+                            />
+                            <FaQuoteLeft className="my-16 w-32 h-32" />
+                            <p className="py-8">{review.details}</p>
+                            <h3 className="text-2xl text-orange-400">{review.name}</h3>
+                        </div>
+                    </SwiperSlide>)
+                }
+            </Swiper>
+        </section>
+    );
+};
+
+export default Testimonials;
