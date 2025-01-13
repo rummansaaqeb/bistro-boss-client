7a277313ec3d8c376bc68ac9b83c43e67ea6295f
rummansaaqeb <rsaaqeb@gmail.com>
2025-01-13 15:06:42 +0600

    added dynamic page title by route using react helmet asyncc

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -7,10 +7,14 @@ import Testimonials from '../Testimonials/Testimonials';
 import BistroBoss from '../BistroBoss/BistroBoss';
 import CallUs from '../CallUs/CallUs';
 import ChefRecommends from '../ChefRecommends/ChefRecommends';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Home</title>
+            </Helmet>
             <Banner></Banner>
             <Category></Category>
             <BistroBoss></BistroBoss>
