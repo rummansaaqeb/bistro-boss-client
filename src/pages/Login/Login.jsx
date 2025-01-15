f552cc1f3cc15643eab34b448da0f37d860c9819
rummansaaqeb <rsaaqeb@gmail.com>
2025-01-15 21:52:57 +0600

    implemented private route and redirect user after login using location

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useRef, useState } from 'react';
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 import { AuthContext } from '../../providers/AuthProvider';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import Swal from 'sweetalert2';
 
@@ -9,6 +9,9 @@ const Login = () => {
     const [disabled, setDisabled] = useState(true);
     const { signIn } = useContext(AuthContext);
     const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || '/';
 
     useEffect(() => {
         loadCaptchaEnginge(6);
@@ -29,7 +32,7 @@ const Login = () => {
                     text: "You Are Now Logged In!",
                     icon: "success"
                 });
-                navigate('/');
+                navigate(from, { replace: true });
             })
     }
 
---
+++ src/pages/Shared/Secret/Secret.jsx
@@ -0,0 +1,10 @@
+
+const Secret = () => {
+    return (
+        <div>
+            <h2>Nothing Secret Here</h2>
+        </div>
+    );
+};
+
+export default Secret;
