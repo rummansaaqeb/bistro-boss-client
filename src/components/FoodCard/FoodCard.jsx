a23ade9e2a4c0b325df59a3ea68eeb98c4a7db2b
rummansaaqeb <rsaaqeb@gmail.com>
2025-01-16 14:51:33 +0600

    enforced user to login before add to cart

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,9 +1,33 @@
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+
 
 const FoodCard = ({ item }) => {
 
     const { name, recipe, image, price } = item;
+    const { user } = useAuth();
+    const navigate = useNavigate();
+
     const handleAddToCart = (food) => {
-        console.log(food);
+        if (user && user.email) {
+            // send cart item to database
+        }
+        else {
+            Swal.fire({
+                title: "You are not logged in",
+                text: "Please login to add to the cart?",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, Login!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login');
+                }
+            });
+        }
     }
 
     return (
