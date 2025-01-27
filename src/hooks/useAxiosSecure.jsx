f21ad067294cf4091a3297a9564c98e0f79e0619
rummansaaqeb <rsaaqeb@gmail.com>
2025-01-27 12:46:21 +0600

    checked admin role using useAdmin custom hook

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,12 +1,16 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+    const navigate = useNavigate();
+    const { logOut } = useAuth();
     axiosSecure.interceptors.request.use(function (config) {
         const token = localStorage.getItem('access-token');
-        console.log('request stopped by interceptors', token);
+        // console.log('request stopped by interceptors', token);
         config.headers.authorization = `Bearer ${token}`;
         return config;
     }, function (error) {
@@ -15,11 +19,16 @@ const useAxiosSecure = () => {
 
 
     // intercepts 401 and 403 status
-    axiosSecure.interceptors.request.use(function (response) {
+    axiosSecure.interceptors.response.use(function (response) {
         return response;
-    }, (error) => {
+    }, async (error) => {
         const status = error.response.status;
-        console.log('status error in the interceptor', status);
+        // console.log('status error in the interceptor', status);
+        // for 401 or 403 logout the user and move the user to the login
+        if (status === 401 || status === 403) {
+            await logOut();
+            navigate('/login');
+        }
         return Promise.reject(error);
     });
 
