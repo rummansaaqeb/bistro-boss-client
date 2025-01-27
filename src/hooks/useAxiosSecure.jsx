e3cd72b5073a7b7ad698cb848d2cd7443180dfce
rummansaaqeb <rsaaqeb@gmail.com>
2025-01-27 12:26:51 +0600

    used axios interceptors in axiosSecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,8 +2,26 @@ import axios from "axios";
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
-}) 
+})
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token');
+        console.log('request stopped by interceptors', token);
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+    }, function (error) {
+        return Promise.reject(error);
+    });
+
+
+    // intercepts 401 and 403 status
+    axiosSecure.interceptors.request.use(function (response) {
+        return response;
+    }, (error) => {
+        const status = error.response.status;
+        console.log('status error in the interceptor', status);
+        return Promise.reject(error);
+    });
 
     return axiosSecure;
 };
